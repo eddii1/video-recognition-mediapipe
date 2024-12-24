fd559c3a2fb6c62f53656c80e52790232a7055f6
Edi <tudoranedi@gmail.com>
2024-12-24 15:09:36 +0200

    counter w logout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import {useState} from 'react';
 
-function Counter(){
-  const [count, setCount] = useState(3);
-
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count+1)}>Increment</button>
-    </div>
-  );
-}
-
-function Salutare(persoana){
-  return <h1>Salut, {persoana.nume}</h1>
-}
-
 function App(){
-  const isLoggedIn = true;
-  const handleClick = () => {
-    alert('Buton APasat');
+  const pas= 3;
+  const [count, setCount] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const checkCounter = () => {
+    if (count === pas-1)
+      setIsLoggedIn(false);
+  };
+
+  const handleIncrement = () => {
+    setCount(count+1);
+    checkCounter();
   };
 
   return(
     <div>
-      {isLoggedIn ? <h1>Bine ai venit</h1> : <h1>Log</h1>}
-      <button onClick={handleClick}>Click me</button>
-      <Salutare  nume="Rizea" />
-      <Counter/>
-      <p>DDDDDDD</p>
+      {isLoggedIn ? (
+        <div>
+          <p>Count: {count}</p>
+          <button onClick={handleIncrement}>Increment</button>
+        </div>
+      ) : (
+        <h1>You have been logged out!</h1>
+      )}
     </div>
   );
 }
+
 
 export default App;
