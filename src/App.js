6d9b6dff23564ac7090eae0984a8b69ef850381e
Edi <tudoranedi@gmail.com>
2024-12-27 12:14:34 +0200

    folosit obiect pt rps

--- src/App.js
+++ src/App.js
@@ -7,16 +7,29 @@ const options = {
 };
 
 function App() {
+  
+  const [selectedValue, setSelectedValue] = useState("");
 
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setSelectedValue(value);
+    console.log(Object.keys(options).find((key) => options[key]===1));
+
+  }
+
+  
   return (
+    <div>
       <select value={selectedValue} onChange={handleChange}>
         <option value="">Select an option</option>
-        {Object.keys(options).map((key) => {
+        {Object.keys(options).map((key) => (
           <option key={key} value={key}>
             {key}
           </option>
-        })}
+        ))}
       </select>
+        <p>Selectat: {selectedValue}</p>
+      </div>
   );
 }
 
