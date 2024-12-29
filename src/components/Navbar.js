9b8a25e0434f88a690e6e4778d4f599e35b6c2ab
Edi <tudoranedi@gmail.com>
2024-12-29 18:38:51 +0200

    DA

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className='bg-slate-200 px-14 font-semibold py-2 grid grid-cols-2 text-xl '>
+      <ul className='flex gap-10'>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
       </ul>
+      <button className='flex justify-end mr-10'>Sign In</button>
     </nav>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function About(){
-    return <h1>About page</h1>;
+    return <h1 className='text-2xl flex justify-center p-3'>About page</h1>;
   }
 
 export default About;
