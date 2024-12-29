4374ef5a32bfe7e8efbfd60ef1f53803b04cbb20
Edi <tudoranedi@gmail.com>
2024-12-29 15:37:06 +0200

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
 
-
-
-function About(){
-  return <h1>About page</h1>;
-}
 
 function App() {
   return (
     <>
-      <Navbar />
-      
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>      
     </>
   );
 }
