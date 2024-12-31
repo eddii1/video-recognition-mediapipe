5cc09ddfe72927a95d6aa98ff7903576ca087e85
Edi <tudoranedi@gmail.com>
2024-12-31 13:26:21 +0200

    adaugat firebase si initializat pt auth

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB49u4GdyZP0PuXgtwxoYJQ4Gv2vaXqNsQ",
+  authDomain: "authmediapipe.firebaseapp.com",
+  projectId: "authmediapipe",
+  storageBucket: "authmediapipe.firebasestorage.app",
+  messagingSenderId: "1046631219902",
+  appId: "1:1046631219902:web:403f4c62cd3c29b3ec94ef",
+  measurementId: "G-YBMLTP72CD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
