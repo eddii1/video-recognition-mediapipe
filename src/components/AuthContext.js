44a7aa6398dfb32c5469be7f525b795b21b7fb24
Edi <tudoranedi@gmail.com>
2025-01-05 17:53:57 +0200

    auth prov

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -3,6 +3,32 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const AuthContext= createContext(null);
 
+export function useAuth(){
+    return useContext(AuthContext);
+}
+
 export function AuthProvider ( { children } ) {
-    const [currUser]
+    const [currUser, setCurrUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect( () => {
+        const auth = getAuth();
+        const unsub = onAuthStateChanged(auth, (user) => {
+            setCurrUser(user);
+            setLoading(false);
+        });
+
+        return () => unsub();
+    })
+
+    const value = {
+        currUser, 
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading ?  children : <p className='p-10 text-3xl'>Loading...</p>}
+        </AuthContext.Provider>
+    );
+
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { AuthProvider } from "./components/AuthContext";
 
 //require('dotenv').config();
 
@@ -28,7 +29,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
