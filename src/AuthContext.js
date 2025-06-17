a65557989813e055098a58c06e8eecd1b59adc55
Tudoran Eduard <tudoranedi@gmail.com>
2025-06-18 02:27:57 +0300

    d

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const AuthContext= createContext(null);
